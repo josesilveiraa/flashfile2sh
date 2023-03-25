2316b3e28eaba4cafdb8027d5e93862fdf4cb430
josesilveiraa <josesilveiraa@protonmail.com>
2023-03-25 15:04:34 -0300

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Command } from 'commander';
+import figlet from 'figlet';
+import generate from './lib/generate.js';
+
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .description('A CLI utility for generating flash scripts from flashfile.xml files.')
+  .option('-i, --input [input]', 'input flashfile.xml file', 'flashfile.xml')
+  .option('-o, --output [output]', 'output flash script file', 'flashfile.sh')
+  .parse(process.argv);
+
+const options = program.opts();
+
+console.log(figlet.textSync('Flashfile2Sh'));
+
+generate(options.input, options.output).then(() => {
+  console.log(`Wrote flashscript to ${options.output}`);
+});
---
+++ src/lib/generate.ts
@@ -0,0 +1,63 @@
+import { XMLParser } from 'fast-xml-parser';
+
+interface Step {
+  '@_operation': string;
+  '@_var'?: string;
+  '@_MD5'?: string;
+  '@_filename'?: string;
+  '@partition'?: string;
+  '@_SHA1'?: string;
+}
+
+export default async function generate(input: string, output: string) {
+  const text = await Bun.file(input).text();
+  const parser = new XMLParser({ ignoreAttributes: false });
+
+  let commandsToBeWritten: string[] = [];
+  let checksumString: string | null = null;
+  let md5sums: Record<string, string> = {};
+  let sha1sums: Record<string, string> = {};
+
+  const { flashing } = parser.parse(text);
+  const steps: Step[] = flashing.steps.step;
+
+  console.log(`Generating shellscript for ${flashing.header.software_version['@_version']}`);
+
+  steps.forEach((step) => {
+    if(step['@_MD5'] && step['@_filename']) {
+      md5sums[step['@_filename']] = step['@_MD5'];
+    } else if(step['@_SHA1'] && step['@_filename']) {
+      sha1sums[step['@_filename']] = step['@_SHA1'];
+    }
+    
+
+    const operation = step['@_operation'];
+    let variables: string[] = [];
+
+    switch(operation) {
+      case 'flash':
+        variables = ['@_operation', '@_partition', '@_filename'];
+        break;
+      
+      case 'erase':
+        variables = ['@_operation', '@_partition'];
+        break;
+
+      case 'getvar':
+      case 'oem':
+        variables = ['@_operation', '@_var'];
+        break;
+    }
+
+    const commandArgs = variables.map(v => step[v as keyof Step] as string).join(' ');
+    commandsToBeWritten.push(`fastboot ${commandArgs} || exit 1\n`);
+  });
+
+  for (const [checker, checksums] of [['md5sum', md5sums], ['sha1sum', sha1sums]]) {
+    if (Object.keys(checksums).length > 0) {
+      checksumString = checker + ' --check <<EOF || exit 1\n' + Object.entries(checksums).map(([filename, checksum]) => `${checksum} *${filename}\n`).join('') + 'EOF';
+    }
+  }
+  commandsToBeWritten.unshift(`${checksumString}\n\n`);
+  Bun.write(output, commandsToBeWritten);
+}
